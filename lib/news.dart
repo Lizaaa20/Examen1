140886bddd9500a7e5d10b502812727dfd6dff50
Lizaaa20 <lizavalentinatorresmena@gmail.com>
2024-08-17 17:30:38 -0600

    Update news.dart

--- lib/news.dart
+++ lib/news.dart
@@ -1,1 +1,11 @@
+class News {
+  final String title;
+  final String description;
+  final String imageUrl;
 
+  News({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+}
